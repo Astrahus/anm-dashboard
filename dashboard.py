88c2901d447b79d225ac86b596b1bddfb4a1d988
Vinicius <vinikuhlmann@gmail.com>
2024-04-04 19:50:29 -0300

    Upgrade

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,12 @@
 import json
 
+from collections import namedtuple
 import pandas as pd
 import streamlit as st
 
-from plots import Barplot, Pieplot, Scatter
+from filtro import Filtro
+
+import plotly.express as px
 
 st.set_page_config(
     page_title="Dashboard SIGMINE",
@@ -11,15 +14,13 @@ st.set_page_config(
     layout="wide",
 )
 
+st.markdown("# Dashboard SIGMINE<br>", unsafe_allow_html=True)
 
+
+@st.cache_resource
 def estabelecer_conexao():
     conn = st.connection("postgresql", type="sql")
     return conn
-
-
-def baixar_dados(conn):
-    query: pd.DataFrame = conn.query("SELECT * FROM sigmine_view", show_spinner=False)
-    return query
 
 
 @st.cache_data(show_spinner=False)
@@ -30,276 +31,304 @@ def carregar_grupos():
 
 
 @st.cache_data(show_spinner="Baixando e processando dados...")
-def processar_dados(grupos) -> pd.DataFrame:
+def baixar_processos() -> pd.DataFrame:
     conn = estabelecer_conexao()
-    query = baixar_dados(conn)
+    query: pd.DataFrame = conn.query("SELECT * FROM processos", show_spinner=False)
 
     query.fase = pd.Categorical(query.fase, ordered=True)
     query.uf = pd.Categorical(query.uf, ordered=True)
     query.subs = pd.Categorical(query.subs)
 
+    grupos = carregar_grupos()
     query.nome = query.nome.replace(grupos)
-
-    quantidade_dms = query.shape[0]
-    area_total = query["area_ha"].sum()
-    quantidade_empresas = query["nome"].nunique()
-
-    return query, quantidade_dms, area_total, quantidade_empresas
+    return query
 
 
-grupos = carregar_grupos()
-dados_processados, quantidade_dms, area_total, quantidade_empresas = processar_dados(
-    grupos
-)
+@st.cache_data(show_spinner=False)
+def baixar_titulares():
+    conn = estabelecer_conexao()
+    query = conn.query("SELECT * FROM processos_titulares", show_spinner=False)
+    return query
 
 
-st.markdown("# Dashboard SIGMINE<br>", unsafe_allow_html=True)
+def filtrar_titulares(processos):
+    titulares = baixar_titulares()
+    return titulares.merge(processos, on=["numero", "ano"])
 
 
-def set_fases():
-    for i, _ in enumerate(dados_processados.fase.cat.categories):
-        st.session_state[f"fase_{i}"] = True
+processos = baixar_processos()
+filtro = Filtro(processos)
 
 
-def reset_fases():
-    for i, _ in enumerate(dados_processados.fase.cat.categories):
-        st.session_state[f"fase_{i}"] = False
+Stats = namedtuple("Stats", ["quantidade", "dms", "area"])
 
 
-def set_uf():
-    for i, _ in enumerate(dados_processados.uf.cat.categories):
-        st.session_state[f"uf_{i}"] = True
+def get_agrupados(processos, filtro):
+    mask = processos.uf.isin(filtro.ufs)
+    filtrados = processos[mask]
 
+    agrupado_todos = (
+        filtrados.groupby("nome")
+        .agg(
+            quantidade_dms_todos=("nome", "count"),
+            area_total_todos=("area_ha", "sum"),
+        )
+        .rename(
+            index={"nome": "Nome da empresa"},
+            columns={
+                "quantidade_dms_todos": "Quantidade de DMs",
+                "area_total_todos": "Área total",
+            },
+        )
+    )
+    quantidade_todos = agrupado_todos.shape[0]
+    quantidade_dms_todos = agrupado_todos["Quantidade de DMs"].sum()
+    area_total_todos = agrupado_todos["Área total"].sum()
 
-def reset_uf():
-    for i, _ in enumerate(dados_processados.uf.cat.categories):
-        st.session_state[f"uf_{i}"] = False
+    if filtro.usando_titulares:
+        filtrados = filtrar_titulares(filtrados)
+    else:
+        fase_mask = processos.fase.isin(filtro.fases)
+        filtrados = processos[fase_mask]
 
-
-# SIDEDBAR
-with st.sidebar:
-    st.markdown("# Configurações")
-    with st.popover("Fases", use_container_width=True):
-        st.markdown("### Fases")
-        ncols = 3
-        uf_cols = st.columns(ncols)
-        filtro_fases = {}
-        TITULADOS = {
-            "AUTORIZAÇÃO DE PESQUISA",
-            "CONCESSÃO DE LAVRA",
-            "DIREITO DE REQUERER A LAVRA",
-            "LAVRA GARIMPEIRA",
-            "LICENCIAMENTO",
-            "REGISTRO DE EXTRAÇÃO",
-            "REQUERIMENTO DE LAVRA",
-        }
-        for i, fase in enumerate(dados_processados.fase.cat.categories):
-            filtro_fases[fase] = uf_cols[i % ncols].checkbox(
-                fase, value=(fase in TITULADOS), key=f"fase_{i}"
-            )
-        st.button("Selecionar todas", on_click=set_fases)
-        st.button("Desmarcar todas", on_click=reset_fases)
-
-    with st.popover("Estados", use_container_width=True):
-        st.markdown("### Estados")
-        ncols = 4
-        uf_cols = st.columns(ncols)
-        filtro_uf = {}
-        for i, uf in enumerate(dados_processados.uf.cat.categories):
-            filtro_uf[uf] = uf_cols[i % ncols].checkbox(uf, value=True, key=f"uf_{i}")
-        st.button("Selecionar todos", on_click=set_uf)
-        st.button("Desmarcar todos", on_click=reset_uf)
-
-    filtro_quantidade = st.number_input(
-        "Quantidade de empresas",
-        min_value=1,
-        max_value=100,
-        value=20,
+    agrupado_filtrados = (
+        filtrados.groupby("nome")
+        .agg(
+            quantidade_dms_filtrados=("nome", "count"),
+            area_total_filtrados=("area_ha", "sum"),
+        )
+        .rename(
+            index={"nome": "Nome da empresa"},
+            columns={
+                "quantidade_dms_filtrados": "Quantidade de DMs",
+                "area_total_filtrados": "Área total",
+            },
+        )
     )
 
-    filtro_tipo_total = st.radio(
-        "'Outras empresas' se refere a:",
-        ["Dados filtrados", "Todos os dados"],
+    quantidade_filtro = agrupado_filtrados.shape[0]
+    quantidade_dms_filtro = agrupado_filtrados["Quantidade de DMs"].sum()
+    area_total_filtro = agrupado_filtrados["Área total"].sum()
+
+    agrupados = (
+        agrupado_filtrados.join(
+            agrupado_todos, how="outer", lsuffix=" (Filtrados)", rsuffix=" (Todos)"
+        )
+        .fillna(0)
+        .rename_axis("Nome da empresa")
+        .sort_values(by="Área total (Filtrados)", ascending=False)
     )
 
-    filtro_altura = 30 * filtro_quantidade
-
-    filtro_incluir_outras = st.checkbox("Incluir 'Outras empresas' nos gráficos")
-
-mask_fases = dados_processados.fase.isin(
-    {fase for fase, filtro in filtro_fases.items() if filtro}
-)
-mask_uf = dados_processados.uf.isin({uf for uf, filtro in filtro_uf.items() if filtro})
-dados_processados = dados_processados[mask_fases & mask_uf].drop_duplicates(
-    subset=("numero", "ano")
-)
-
-dados_agrupados = (
-    dados_processados.groupby("nome")
-    .agg(
-        quantidade=("nome", "count"),
-        area_total=("area_ha", "sum"),
+    return (
+        Stats(quantidade_todos, quantidade_dms_todos, area_total_todos),
+        Stats(quantidade_filtro, quantidade_dms_filtro, area_total_filtro),
+        agrupados,
     )
-    .sort_values(by="area_total", ascending=False)
-)
-quantidade_dms_filtro = dados_agrupados["quantidade"].sum()
-area_total_filtro = dados_agrupados["area_total"].sum()
-quantidade_empresas_filtro = dados_agrupados.shape[0]
 
 
-df_quantidade = (
-    dados_agrupados[["quantidade"]]
-    .sort_values(by="quantidade", ascending=False)
-    .head(filtro_quantidade)
-    .reset_index()
-)
-df_quantidade["nome"] = df_quantidade["nome"].str[:30]
-
-df_area = dados_agrupados[["area_total"]].head(filtro_quantidade).reset_index()
-df_area["nome"] = df_area["nome"].str[:30]
+stats_todos, stats_filtro, agrupados = get_agrupados(processos, filtro)
 
 
-if filtro_incluir_outras:
-    if filtro_tipo_total == "Dados filtrados":
-        df_quantidade.loc[filtro_quantidade] = [
-            "Outras",
-            quantidade_dms_filtro - df_quantidade["quantidade"].sum(),
-        ]
-        df_area.loc[filtro_quantidade] = [
-            "Outras",
-            area_total_filtro - df_area["area_total"].sum(),
-        ]
-    elif filtro_tipo_total == "Todos os dados":
-        df_quantidade.loc[filtro_quantidade] = [
-            "Outras",
-            quantidade_dms - df_quantidade["quantidade"].sum(),
-        ]
-        df_area.loc[filtro_quantidade] = [
-            "Outras",
-            area_total - df_area["area_total"].sum(),
-        ]
-
-# METRICAS
 with st.container(border=True):
-    top_n_quantidade = df_quantidade.iloc[:filtro_quantidade]["quantidade"].sum()
-    top_n_area = df_area.iloc[:filtro_quantidade]["area_total"].sum()
-
-    if filtro_tipo_total == "Dados filtrados":
-        quantidade_outras = quantidade_empresas_filtro - top_n_quantidade
-        quantidade_dms_outras = quantidade_dms_filtro - top_n_quantidade
-        area_outras = area_total_filtro - top_n_area
-    elif filtro_tipo_total == "Todos os dados":
-        quantidade_outras = quantidade_empresas - filtro_quantidade
-        quantidade_dms_outras = quantidade_dms - top_n_quantidade
-        area_outras = area_total - top_n_area
 
     col1, col2, col3 = st.columns(3)
     col1.metric(
-        label="Total de DMs",
-        value=f"{round(quantidade_dms / 1000, 2)}k",
+        label="Quantidade total de empresas",
+        value=f"{stats_todos.quantidade}",
     )
     col2.metric(
-        label="Total de empresas",
-        value=f"{quantidade_empresas}",
+        label="Quantidade total de DMs",
+        value=f"{round(stats_todos.dms / 1000, 2)}k",
     )
+
     col3.metric(
         label="Área total",
-        value=f"{round(area_total / 1e6, 2)}M ha",
+        value=f"{round(stats_todos.area / 1e6, 2)}M ha",
     )
 
-    col1, col2 = st.columns(2)
-    with col1:
-        col11, col12 = st.columns(2)
-        col11.metric(
-            label=f"Top {filtro_quantidade} empresas possuem",
-            value=f"{round(top_n_quantidade / 1000, 2)}k DMs ({top_n_quantidade / quantidade_dms * 100:.2f}%)",
-        )
-        col12.metric(
-            label=f"Outras {quantidade_outras} empresas possuem",
-            value=f"{round(quantidade_dms_outras / 1000, 2)}k DMs ({quantidade_dms_outras / quantidade_dms * 100:.2f}%)",
-        )
-    with col2:
-        col21, col22 = st.columns(2)
-        col21.metric(
-            label=f"Top {filtro_quantidade} empresas possuem",
-            value=f"{round(top_n_area / 1e6, 2)}M ha ({top_n_area / area_total * 100:.2f}%)",
-        )
-        col22.metric(
-            label=f"Outras {quantidade_outras} empresas possuem",
-            value=f"{round(area_outras / 1e6, 2)}M ha ({area_outras / area_total * 100:.2f}%)",
-        )
+    col1.metric(
+        label="Quantidade de empresas nas fases filtradas",
+        value=f"{stats_filtro.quantidade}",
+    )
+    col2.metric(
+        label="Quantidade de DMs nas fases filtradas",
+        value=f"{round(stats_filtro.dms / 1000, 2)}k",
+    )
 
-if not filtro_incluir_outras:
-    st.plotly_chart(
-        Scatter.plot(
-            dados_agrupados.head(filtro_quantidade),
-            "quantidade",
-            "area_total",
-            "Área x Quantidade",
-            "Área (ha)",
-            "Quantidade",
-        ),
+    col3.metric(
+        label="Área total nas fases filtradas",
+        value=f"{round(stats_filtro.area / 1e6, 2)}M ha",
+    )
+
+
+def get_top(agrupados):
+    top_dms = agrupados["Quantidade de DMs (Filtrados)"].nlargest(filtro.quantidade)
+    top_area = agrupados["Área total (Filtrados)"].nlargest(filtro.quantidade)
+    top_dms_todos = agrupados["Quantidade de DMs (Todos)"].loc[top_dms.index]
+    top_area_todos = agrupados["Área total (Todos)"].loc[top_area.index]
+
+    top_dms.rename("Quantidade de DMs", inplace=True)
+    top_area.rename("Área total", inplace=True)
+    top_dms_todos.rename("Quantidade de DMs", inplace=True)
+    top_area_todos.rename("Área total", inplace=True)
+
+    top_dms = top_dms.reset_index()
+    top_area = top_area.reset_index()
+    top_dms_todos = top_dms_todos.reset_index()
+    top_area_todos = top_area_todos.reset_index()
+
+    top_dms["Tipo"] = "Filtrados"
+    top_area["Tipo"] = "Filtrados"
+    top_dms_todos["Tipo"] = "Todos"
+    top_area_todos["Tipo"] = "Todos"
+
+    top_dms = pd.concat([top_dms_todos, top_dms], join="inner")
+    top_area = pd.concat([top_area_todos, top_area], join="inner")
+
+    # reduce nome da empresa size
+    top_dms["Nome da empresa"] = top_dms["Nome da empresa"].str[:30]
+    top_area["Nome da empresa"] = top_area["Nome da empresa"].str[:30]
+
+    return top_dms, top_area
+
+
+top_dms, top_area = get_top(agrupados)
+
+with st.container(border=True):
+    col1, col2 = st.columns(2)
+
+    st.markdown("## Gráficos de barras")
+
+    fig = px.bar(
+        data_frame=top_dms,
+        x="Quantidade de DMs",
+        y="Nome da empresa",
+        color="Tipo",
+        barmode="group",
+        title=f"Top {filtro.quantidade} empresas por quantidade de DMs",
+        text_auto=True,
+        template="plotly_white",
+    )
+    fig.update_layout(
+        height=filtro.altura, yaxis={"autorange": "reversed", "title": ""}
+    )
+    col1.plotly_chart(
+        fig,
         use_container_width=True,
     )
-else:
-    st.plotly_chart(
-        Scatter.plot(
-            dados_agrupados.head(1000),
-            "quantidade",
-            "area_total",
-            "Área x Quantidade",
-            "Área (ha)",
-            "Quantidade",
-        ),
+
+    fig = px.bar(
+        data_frame=top_area,
+        x="Área total",
+        y="Nome da empresa",
+        color="Tipo",
+        barmode="group",
+        title=f"Top {filtro.quantidade} empresas por área total",
+        text_auto=True,
+        template="plotly_white",
+    )
+    fig.update_layout(
+        height=filtro.altura, yaxis={"autorange": "reversed", "title": ""}
+    )
+    col2.plotly_chart(
+        fig,
         use_container_width=True,
     )
 
-# GRÁFICOS DE BARRAS
-with st.container():
+with st.container(border=True):
     col1, col2 = st.columns(2)
-    with col1:
-        st.plotly_chart(
-            Barplot.plot(
-                df_quantidade,
-                "quantidade",
-                "nome",
-                "Quantidade de DMs",
-                "Quantidade",
-                filtro_altura,
-            ),
-            use_container_width=True,
-        )
 
-    with col2:
-        st.plotly_chart(
-            Barplot.plot(
-                df_area,
-                "area_total",
-                "nome",
-                "Área total",
-                "Área (ha)",
-                filtro_altura,
-            ),
-            use_container_width=True,
-        )
+    st.markdown("## Gráficos de pizza")
 
-# GRÁFICOS DE PIZZA
-with st.container():
-    col1, col2 = st.columns(2)
-    with col1:
-        st.plotly_chart(
-            Pieplot.plot(df_quantidade, "nome", "quantidade", "Proporção de DMs"),
-            use_container_width=True,
-        )
-    with col2:
-        st.plotly_chart(
-            Pieplot.plot(df_area, "nome", "area_total", "Proporção de área"),
-            use_container_width=True,
-        )
+    fig = px.pie(
+        data_frame=top_dms[top_dms["Tipo"] == "Todos"],
+        names="Nome da empresa",
+        values="Quantidade de DMs",
+        hole=0.5,
+        title=f"Top {filtro.quantidade} empresas por quantidade de DMs (Todos)",
+        template="plotly_white",
+    )
+    fig.update_layout(height=filtro.altura / 2)
+    col1.plotly_chart(
+        fig,
+        use_container_width=True,
+    )
+
+    fig = px.pie(
+        data_frame=top_area[top_area["Tipo"] == "Todos"],
+        names="Nome da empresa",
+        values="Área total",
+        hole=0.5,
+        title=f"Top {filtro.quantidade} empresas por área (Todos)",
+        template="plotly_white",
+    )
+
+    fig.update_layout(height=filtro.altura / 2)
+    col2.plotly_chart(
+        fig,
+        use_container_width=True,
+    )
+
+    fig = px.pie(
+        data_frame=top_dms[top_dms["Tipo"] == "Filtrados"],
+        names="Nome da empresa",
+        values="Quantidade de DMs",
+        hole=0.5,
+        title=f"Top {filtro.quantidade} empresas por quantidade de DMs (Filtrados)",
+        template="plotly_white",
+    )
+
+    fig.update_layout(height=filtro.altura / 2)
+    col1.plotly_chart(
+        fig,
+        use_container_width=True,
+    )
+
+    fig = px.pie(
+        data_frame=top_area[top_area["Tipo"] == "Filtrados"],
+        names="Nome da empresa",
+        values="Área total",
+        hole=0.5,
+        title=f"Top {filtro.quantidade} empresas por área (Filtrados)",
+        template="plotly_white",
+    )
+    fig.update_layout(height=filtro.altura / 2)
+    col2.plotly_chart(
+        fig,
+        use_container_width=True,
+    )
 
 
-# TABELA DE DADOS FILTRADOS
-st.markdown("## Dados filtrados")
-col1, col2 = st.columns([4, 1])
-col1.write(dados_processados)
-col2.write(dados_agrupados)
+@st.cache_data
+def convert_df(df: pd.DataFrame) -> str:
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv()
+
+
+with st.container(border=True):
+    st.markdown("## Tabelas de dados")
+
+    st.markdown("### Dados agrupados")
+    st.dataframe(agrupados, use_container_width=True)
+    csv = convert_df(agrupados)
+    st.download_button(
+        label="Download CSV",
+        data=csv,
+        file_name="agrupados.csv",
+        mime="text/csv",
+    )
+
+    st.markdown("### Processos")
+    processos = baixar_processos()
+    processos.columns = processos.columns.str.capitalize()
+    processos["Processo"] = processos["Numero"] + "/" + processos["Ano"]
+    processos = processos.drop(columns=["Numero", "Ano"])
+    processos.set_index("Processo", inplace=True)
+    st.dataframe(processos, use_container_width=True)
+    csv = convert_df(processos)
+    st.download_button(
+        label="Download CSV",
+        data=csv,
+        file_name="processos.csv",
+        mime="text/csv",
+    )
---
+++ filtro.py
@@ -0,0 +1,113 @@
+import streamlit as st
+
+TITULADOS = {
+    "AUTORIZAÇÃO DE PESQUISA",
+    "CONCESSÃO DE LAVRA",
+    "DIREITO DE REQUERER A LAVRA",
+    "LAVRA GARIMPEIRA",
+    "LICENCIAMENTO",
+    "REGISTRO DE EXTRAÇÃO",
+    "REQUERIMENTO DE LAVRA",
+}
+
+
+class Callbacks:
+    fases: list = None
+    ufs: list = None
+
+    @staticmethod
+    def configure(processos):
+        Callbacks.fases = processos.fase.cat.categories
+        Callbacks.ufs = processos.uf.cat.categories
+
+    @staticmethod
+    def set_fases_all():
+        for i, _ in enumerate(Callbacks.fases):
+            st.session_state[f"fase_{i}"] = True
+
+    @staticmethod
+    def set_fases_titulados():
+        for i, fase in enumerate(Callbacks.fases):
+            st.session_state[f"fase_{i}"] = fase in TITULADOS
+
+    @staticmethod
+    def reset_fases():
+        for i, _ in enumerate(Callbacks.fases):
+            st.session_state[f"fase_{i}"] = False
+
+    @staticmethod
+    def set_uf_all():
+        for i, _ in enumerate(Callbacks.ufs):
+            st.session_state[f"uf_{i}"] = True
+
+    @staticmethod
+    def reset_uf():
+        for i, _ in enumerate(Callbacks.ufs):
+            st.session_state[f"uf_{i}"] = False
+
+
+class Filtro:
+
+    def __init__(self, processos):
+        Callbacks.configure(processos)
+        with st.sidebar:
+            self.fases = self.criar_filtro_fases()
+            self.ufs = self.criar_filtro_estados()
+            self.quantidade = self.criar_filtro_quantidade()
+            self.altura = self.criar_filtro_altura()
+            self.usando_titulares = self.fases == TITULADOS
+
+    def criar_filtro_fases(self):
+        with st.popover("Fases", use_container_width=True):
+            st.markdown("### Fases")
+            NCOLS = 3
+            cols = st.columns(NCOLS)
+            checkboxes = {}
+            for i, fase in enumerate(Callbacks.fases):
+                if not hasattr(st.session_state, f"fase_{i}"):
+                    st.session_state[f"fase_{i}"] = False
+                checkboxes[fase] = cols[i % NCOLS].checkbox(
+                    fase,
+                    fase in TITULADOS,
+                    key=f"fase_{i}",
+                )
+            col1, col2, col3 = st.columns(3)
+            col1.button("Selecionar todas", on_click=Callbacks.set_fases_all)
+            col2.button("Selecionar titulados", on_click=Callbacks.set_fases_titulados)
+            col3.button("Desmarcar todas", on_click=Callbacks.reset_fases)
+
+        return {fase for fase, marcado in checkboxes.items() if marcado}
+
+    def criar_filtro_estados(self):
+        with st.popover("Estados", use_container_width=True):
+            st.markdown("### Estados")
+            NCOLS = 4
+            uf_cols = st.columns(NCOLS)
+            checkboxes = {}
+            for i, uf in enumerate(Callbacks.ufs):
+                if not hasattr(st.session_state, f"uf_{i}"):
+                    st.session_state[f"uf_{i}"] = False
+                checkboxes[uf] = uf_cols[i % NCOLS].checkbox(
+                    uf,
+                    value=True,
+                    key=f"uf_{i}",
+                )
+            col1, col2 = st.columns(2)
+            col1.button("Selecionar todos", on_click=Callbacks.set_uf_all)
+            col2.button("Desmarcar todos", on_click=Callbacks.reset_uf)
+
+        estados = {uf for uf, marcado in checkboxes.items() if marcado}
+        return estados
+
+    def criar_filtro_quantidade(self):
+        quantidade = st.number_input(
+            "Quantidade de empresas nos gráficos",
+            min_value=1,
+            max_value=100,
+            value=20,
+        )
+
+        return quantidade
+
+    def criar_filtro_altura(self):
+        return 60 * self.quantidade
